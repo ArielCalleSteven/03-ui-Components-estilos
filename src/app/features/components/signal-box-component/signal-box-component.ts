3e1e79144758a38832dfd77c0915c6ab8a0ed4d2
ArielCalleSteven <a.calleduma123@gmail.com>
2025-11-21 17:44:34 -0500

    estilos completos A1

--- src/app/features/components/signal-box-component/signal-box-component.ts
+++ src/app/features/components/signal-box-component/signal-box-component.ts
@@ -10,11 +10,21 @@ import { CommonModule } from '@angular/common';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SignalBoxComponent {
+  
+
   valor = signal<number>(0);
 
   cambiarValor(event: Event) {
     const input = event.target as HTMLInputElement;
     const nuevoValor = Number(input.value);
-    this.valor.set(nuevoValor); 
+    this.valor.set(nuevoValor);
+  }
+  
+  progreso = signal<number>(50);
+
+  actualizarProgreso(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const nuevoProgreso = Number(input.value);
+    this.progreso.set(nuevoProgreso);
   }
 }
