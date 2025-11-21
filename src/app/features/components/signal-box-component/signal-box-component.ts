44e1aada7582e4f89c6e9a40469b42a71d91154e
ArielCalleSteven <a.calleduma123@gmail.com>
2025-11-21 17:34:44 -0500

    estilos completos

---
+++ src/app/features/components/signal-box-component/signal-box-component.ts
@@ -0,0 +1,20 @@
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-signal-box',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './signal-box-components.html', 
+  styleUrl: './signal-box-component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SignalBoxComponent {
+  valor = signal<number>(0);
+
+  cambiarValor(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const nuevoValor = Number(input.value);
+    this.valor.set(nuevoValor); 
+  }
+}
