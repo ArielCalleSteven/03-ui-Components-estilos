28cc959d20952d61e4bc03a3eef56f19101bade7
ArielCalleSteven <a.calleduma123@gmail.com>
2025-11-24 18:00:46 -0500

    estiloss simpsons 2

---
+++ src/app/features/simpsons-page/simpsons-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-simpsons-page',
+  imports: [],
+  templateUrl: './simpsons-page.html',
+  styleUrl: './simpsons-page.css',
+})
+export class SimpsonsPage {
+
+}
